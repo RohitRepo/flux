83343d3b0ec853ee637b95043377b46065c02de8
Umesh Sharma <umesh@magicpin.in>
2017-12-20 17:15:42 +0530

    Initial commit with a single page

---
+++ deploy/fabfile.py
@@ -0,0 +1,84 @@
+from fabric.contrib.files import append, exists, sed
+from fabric.api import env, local, run
+import random
+
+REPO_URL = 'https://rohit_magic@bitbucket.org/samast/samara.git'
+
+
+def deploy():
+    site_folder = '/home/%s/sites/samara' % (env.user)
+    source_folder = site_folder + '/source'
+    _create_directory_structure_if_necessary(site_folder)
+    _get_latest_source(source_folder)
+    _update_settings(source_folder, env.host)
+    _update_virtualenv(source_folder)
+    _update_npm(source_folder)
+    _build_client(source_folder)
+    _update_static_files(source_folder)
+    _update_database(source_folder)
+    _restart_service()
+    _copy_celery_config(source_folder)
+    _restart_celery()
+
+
+def _create_directory_structure_if_necessary(site_folder):
+    for subfolder in ('source', 'static', 'database', 'virtualenv', 'media', 'log/gunicorn'):
+        run('mkdir -p %s/%s' % (site_folder, subfolder))
+
+
+def _get_latest_source(source_folder):
+    if exists(source_folder + '/.git'):
+        run('cd %s && git fetch' % (source_folder,))
+    else:
+        run('git clone %s %s' % (REPO_URL, source_folder))
+    current_commit = local("git log -n 1 --format=%H", capture=True)
+    run('cd %s && git reset --hard %s' % (source_folder, current_commit))
+
+
+def _update_settings(source_folder, site_name):
+    settings_path = source_folder + '/samara/settings.py'
+    sed(settings_path, "DEBUG = True", "DEBUG = False")
+    sed(settings_path,
+        'ALLOWED_HOSTS =.+$',
+        'ALLOWED_HOSTS = ["%s", "localhost", "webapi.magicpin.in", "www.webapi.magicpin.in", "*"]' % (site_name,)
+        )
+
+    secret_key_file = source_folder + '/samara/secret_key.py'
+    if not exists(secret_key_file):
+        chars = 'abcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*(-_=+)'
+        key = ''.join(random.SystemRandom().choice(chars) for _ in range(50))
+        append(secret_key_file, "SECRET_KEY = '%s'" % (key,))
+    append(settings_path, '\nfrom .secret_key import SECRET_KEY')
+
+
+def _update_virtualenv(source_folder):
+    virtualenv_folder = source_folder + '/../virtualenv'
+    if not exists(virtualenv_folder + '/bin/pip'):
+        run('virtualenv  --no-site-packages -p python3 %s' % (virtualenv_folder,))
+    run('%s/bin/pip install -r %s/deploy/requirements.txt' % (
+        virtualenv_folder, source_folder))
+
+def _update_npm(source_folder):
+    npm_folder = source_folder + '/samara'
+    run('cd %s && npm install' % (npm_folder,))
+
+def _build_client(source_folder):
+    client_folder = source_folder + '/samara'
+    run('cd %s && npm run build && grunt build' % (client_folder,))
+
+def _update_static_files(source_folder):
+    run('cd %s && ../virtualenv/bin/python manage.py collectstatic --noinput' % (
+        source_folder,))
+
+def _update_database(source_folder):
+    run('cd %s && ../virtualenv/bin/python manage.py migrate --noinput' % (
+        source_folder,))
+
+def _restart_service():
+    run('sudo service samara restart')
+
+def _copy_celery_config(source_folder):
+    run('sudo cp %s/deploy/celeryd_conf /etc/default/celeryd' % (source_folder))
+
+def _restart_celery():
+    run('sudo service celeryd restart')
